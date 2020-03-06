b4dce2257c255668d4d162092152aac3429d5318
Shilpa Vahinipati <svahinipati@infoblox.com>
2020-03-06 21:37:56 +0530

    heroku

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ res.sendFile(path.join(__dirname+'/dist/food-and-drinks/index.html'));
 });
 
 app.listen(process.env.PORT || 4200); 
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
